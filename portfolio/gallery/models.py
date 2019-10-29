0bc929a6f19b58dfdd0e154bf2c104f41b907327
GIT <chenda4701@163.com>
2019-10-29 09:24:52 +0800

    gallery app 4th

--- portfolio/gallery/models.py
+++ portfolio/gallery/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Gallery(models.Model):
     description = models.CharField(default='description', max_length=100)
-    image = models.ImageField(default='default.png', upload_to='images/')
+    image = models.ImageField(default='default.png', upload_to='/images/')
     title = models.CharField(default='Title', max_length=50)
 
     def __str__(self):
