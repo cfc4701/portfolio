adb4e9223dfe647092c67c5d32c7ddc630916bf7
GIT <chenda4701@163.com>
2019-10-29 11:02:28 +0800

    制作blog app

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Personal App
     'gallery.apps.GalleryConfig',
+    'blog',
 ]
 
 MIDDLEWARE = [
